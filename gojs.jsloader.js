b9752404c179ed337cb242269d38dad54988a018
yucong02 <yucong02@baidu.com>
2017-11-07 17:58:05 +0800

    use transform-runtime translate

--- gojs.jsloader.js
+++ gojs.jsloader.js
@@ -10,7 +10,14 @@ module.exports = [
             presets: ['es2015', 'react', 'stage-0'],
             plugins: [
                 // 'react-hot-loader/babel',
-                'transform-decorators-legacy'
+                'transform-decorators-legacy',
+                [
+                    'transform-runtime',
+                    {
+                        'polyfill': false,
+                        'regenerator': true
+                    }
+                ]
             ]
         },
         happy: {id: 'js'}
